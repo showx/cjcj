3a6c0a8a61b6d518bfb57f7bb03364f00b429c27
show <showshou@126.com>
2016-01-11 13:25:29 +0800

    format

---
+++ cls_format.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * 代码format
+ * Author:show(9448923@qq.com)
+ * Date:2016.1.11
+ */
+cls_format
+{
+    /**
+     * json to array
+     */
+    public function jtoa($a)
+    {
+        $a = json_decode($a,true);
+        echo "====================\n";
+        echo "array(\n";
+        foreach($a as $key=>$val)
+        {
+               self::garr($val,"\$arr[$key]"); 
+        }
+       echo ")";
+
+    }
+    public function jtoc($a)
+    {
+        foreach($a as $key=>$val)
+        {
+               self::garr($val,"\$arr[$key]"); 
+        }
+    }
+    /**
+     * 数组配置文件式
+     */    
+    public function xarr($arr,$e='')
+    {
+        if(is_array($arr))
+        {
+            foreach($arr as $k=>$v)
+            {
+              $kk = $e."[".$k."]";
+              self::xarr($v,$kk);
+            }
+        }else{
+            echo $e."=\"".$arr."\";\n";
+        }
+    }  
+    /*
+     * json to array
+     */
+    public function garr($arr,$i=1)
+    {
+        $str = str_repeat(" ",$i);
+        $str2 = str_repeat(" ",$i+1);
+        if(is_array($arr))
+        {
+            echo "\n$str array(\n";
+            foreach($arr as $k=>$v)
+            {
+                echo $str2.'"'.$k."\"=>";
+                if(is_array($v))
+                {
+                    $j = $i+2;
+                }else{
+                    $j = $i;
+                }
+                self::garr($v,$j);
+            }   
+            echo $str."),\n";
+        }else{
+            echo '"'.$arr.'",'."\n";
+        }
+    }
+
+}
